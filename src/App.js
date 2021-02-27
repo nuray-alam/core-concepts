0745d23355f6084cd86d3f6cac7b4c435d60b891
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-28 01:20:37 +0600

    Pass object to components and access object practice done

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
 
-  const Person1 = {
-    name: 'Mahfuz',
-    job: 'Thelagari'
-  };
-  const personStyle = {
-    color: "red",
-    backgroundColor: 'salmon'
-  };
+  const products = [
+    { name: "Photoshop", price: "$90.99" },
+    { name: "Illustrator", price: "$60.99" },
+    { name: "PDF Reader", price: "$6.99" }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>I'm React Person</p>
+        <Product product={products[0]}></Product>
+        <Product product = {products[1]}></Product>
+        <Product product = {products[2]}></Product>
         <Person name="Sakib Al Hasan" profession="Cricketer"></Person>
         <Person name="Jalanta Alil" profession="Actor"></Person>
         <Person name="Imagine Dragons" profession="Music Band"></Person>
@@ -39,6 +38,27 @@ function Person(props) {
     <div style={personStyle}>
       <h1>Name: {props.name}</h1>
       <h3>Profession: {props.profession}</h3>
+    </div>
+  )
+}
+
+
+function Product(props) {
+
+  const productStyle = {
+    border: "1px solid gray",
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+ const {name, price}  = props.product;
+  return (
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
     </div>
   )
 }
