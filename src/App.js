f9b1ff2bdb963795d1a8f4ecdb71cb11dc982e90
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-28 00:52:30 +0600

    Pass dynamic data to components, props in react practice done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
 
-const Person1 = {
-  name: 'Mahfuz',
-  job: 'Thelagari'
-};
-const personStyle = {
-  color: "red",
-  backgroundColor:'salmon'
-};
+  const Person1 = {
+    name: 'Mahfuz',
+    job: 'Thelagari'
+  };
+  const personStyle = {
+    color: "red",
+    backgroundColor: 'salmon'
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,12 +18,29 @@ const personStyle = {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className = "" style = {personStyle}>My first paragraph</p>
-        <p className = "" style = {personStyle}>Name: {Person1.name}</p>
-        <p className = "" style = {personStyle}>Job: {Person1.job}</p>
+        <Person name="Sakib Al Hasan" profession="Cricketer"></Person>
+        <Person name="Jalanta Alil" profession="Actor"></Person>
+        <Person name="Imagine Dragons" profession="Music Band"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  const personStyle = {
+    border: "2px solid black",
+    margin: "10px",
+    padding: "15px",
+    width: "50%",
+    backgroundColor: "#6495ed",
+    color: "black"
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: {props.name}</h1>
+      <h3>Profession: {props.profession}</h3>
+    </div>
+  )
 }
 
 export default App;
