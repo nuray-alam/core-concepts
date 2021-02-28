24f09eb684cc93f3f26793cd46b3470c032adcba
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-28 11:41:30 +0600

    Load dynamic data, API call useEffect integrate state practice done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -24,7 +24,7 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>I'm React Person</p>
         <Count></Count>
-
+        <Users></Users>
         <ul>
           {
             nayoks.map(nayok => <li>{nayok}</li>)
@@ -100,5 +100,25 @@ function Count(props) {
     </div>
   )
 }
+
+function Users() {
+  const [users, setUsers]  = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
 
 export default App;
